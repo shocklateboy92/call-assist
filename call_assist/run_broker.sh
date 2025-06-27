e78b5ad683d09698d10aa7f12eafcab4e4add5f7
Lasath Fernando <devel@lasath.org>
2025-06-27 21:25:51 +0000

    feat: add run_broker script for project setup and linting

---
+++ call_assist/run_broker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Switch to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Do the checking/linting outside the call_assist directory,
+# because it's also installed as an editable package.
+pushd ..
+
+# Type check the project
+mypy call_assist
+
+# lint the project
+ruff check call_assist --fix
+
+popd
+
+# Run the broker
+python3 call_assist.addon.broker
